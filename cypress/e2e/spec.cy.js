a49cae7a127903196e5ce9da2eb8059d826a9f58
jorge <jorgemalaquia@gmail.com>
2022-09-26 02:18:07 -0300

    tests: navigation between different pages is tested

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,31 +1,44 @@
+
+
 describe('testing "/"', () => {
   it('Testing if it goes to the "/top" when the icon is clicked"', () => {
     cy.visit('http://localhost:3000/');
+    cy.get('#buttonTop').click();
+    cy.url().should('equal','http://localhost:3000/top');
   })
   it('Testing if it goes to the "/random" when the icon is clicked"', () => {
     cy.visit('http://localhost:3000/');
-  })
-  it('Testing if a new recommendations is created when the users try to do that through the forms"', () => {
-    cy.visit('http://localhost:3000/');
+    cy.get('#buttonRandom').click();
+    cy.url().should('equal','http://localhost:3000/random');
   })
 })
 
 describe('testing "/top"', () => {
   it('Testing  if it goes to the "/" when the icon is clicked', () => {
     cy.visit('http://localhost:3000/top');
+    cy.get('#buttonHome').click();
+    cy.url().should('equal','http://localhost:3000/');
+    
   })
   it('Testing if it goes to the "/random" when the icon is clicked"', () => {
     cy.visit('http://localhost:3000/');
+    cy.get('#buttonRandom').click();
+    cy.url().should('equal','http://localhost:3000/random');
   })
   // ver se tem como verificar que as recommendations estão em ordem decrescente de scores
 })
 describe('testing "/random"', () => {
   it('Testing  if it goes to the "/" when the icon is clicked', () => {
     cy.visit('http://localhost:3000/top');
+    cy.get('#buttonHome').click();
+    cy.url().should('equal','http://localhost:3000/');
   })
   it('Testing if it goes to the "/top" when the icon is clicked"', () => {
     cy.visit('http://localhost:3000/');
+    cy.get('#buttonTop').click();
+    cy.url().should('equal','http://localhost:3000/top');
   })
+
   //ver se tem como verificar que uma recommendation foi exibida
 })
 
